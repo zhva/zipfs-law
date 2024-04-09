c41b636bbbba3ad5f725330b4e7a7b1eb72dc4ad
Viktoriia Zhuravel <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2024-04-09 16:57:15 +0200

    Add TextCard component

---
+++ src/app/components/TextCard.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { CardWrapper } from "./CardWrapper";
+
+interface TextCardProps {
+    genre: string;
+    color: string;
+}
+
+export const TextCard = ({ genre, color }: TextCardProps) => {
+    return (
+        <StyledCardWrapper color={color}>
+            <Text>
+                {"The correlation between the evaluated word frequency and the values calculated according to  Zipf's Law for "}
+                <b>{genre.toUpperCase()}</b>
+            </Text>
+        </StyledCardWrapper>
+    );
+};
+
+const StyledCardWrapper = styled(CardWrapper)`
+    height: -webkit-fill-available;
+`;
+
+const Text = styled.p`
+    font-size: 20px;
+    padding: 15px;
+    text-align: center;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
